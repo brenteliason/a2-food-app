62c7e282d6d697052e86d337f604371d5fc40710
Brent <brenteliason@gmail.com>
2018-10-23 11:08:09 -0400

    Added SideMenu component

---
+++ src/SideMenu.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class SideMenu extends Component {
+
+  componentDidMount() {
+    console.log("Side menu did mount");
+  }
+
+  render() {
+    return (
+       <div className="options-box">
+         <h1>Find A Restaurant in A2</h1>
+         <div>
+           <input id="show-restaurants" type="button" value="Show Restaurants"/>
+           <input id="hide-restaurants" type="button" value="Hide Restaurants"/>
+         </div>
+         <hr/>
+         <div>
+           <span className="text">Search for restaurants by name</span>
+           <input id="places-search" type="text" placeholder="Ex: Zingerman's Deli"/>
+           <input id="go-places" type="button" value="Go"/>
+         </div>
+         <hr/>
+         <div id="restaurant-list">
+         </div>
+       </div>
+    )
+  }
+}
+
+export default SideMenu
