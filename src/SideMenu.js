03a771dd80837824dbfe3edd0d99967c77200ee2
Brent <brenteliason@gmail.com>
2018-11-01 09:11:08 -0400

    Added aria-label to search bar

--- src/SideMenu.js
+++ src/SideMenu.js
@@ -43,7 +43,7 @@ class SideMenu extends Component {
            <div className="options-box" aria-label="menu">
              <h1>Find Food in A2</h1>
              <div>
-               <input id="restaurant-search" type="text" placeholder="Search for food" value={this.props.query}
+               <input id="restaurant-search" type="text" aria-label="searchbar for food options" placeholder="Search for food" value={this.props.query}
                 onChange={(event) => this.props.filterRestaurants(event.target.value)}/>
                <input id="go-places" type="button" value="Go"/>
              </div>
